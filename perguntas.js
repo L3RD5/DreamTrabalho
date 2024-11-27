434de41cc484a8e61d3c154911416cde6ca5acaa
L3RD5 <senhordastrevas20@gmail.com>
2024-11-27 10:46:51 -0300

    Add files via upload
    Fatos curiosos sobre filmes da DreamWorks

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+// Vinicius De Moraes Silva Leal 2D
+criaCartao(
+    'Shrek',
+    'Shrek tem filhos?',
+    'Shrek tem 3 filhos'
+)
+
+criaCartao(
+    'Como Treinar Seu Dragão',
+    'Porque o nome "banguela"?',
+    'O nome vem do livro, onde ele só tem 1 dente'
+)
+
+criaCartao(
+    'Kung Fu Panda',
+    'Pô e a Tigresa namoram?',
+    'NÃO'
+)
+
+criaCartao(
+    'Madagascar',
+    'Porque Alex enfiou uma tora no melmam?',
+    'Para o tirar da caixa'
+)
